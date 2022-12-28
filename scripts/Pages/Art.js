0ba82ddfa4fc3de24bbdd3d1c50b70c5a40be3b2
GroundedWren <120978593+GroundedWren@users.noreply.github.com>
2022-12-28 01:45:36 -0600

    Update gallery

--- scripts/Pages/Art.js
+++ scripts/Pages/Art.js
@@ -366,6 +366,8 @@ registerNamespace("Pages.Art", function (ns)
 		artists = [];
 		// Date the art was created or posted
 		date;
+		// A description of the work
+		description;
 		// Whether the art is explicit, sexually or otherwise
 		__isExplicit;
 		// Whether the frame is currently filtered out
@@ -381,7 +383,7 @@ registerNamespace("Pages.Art", function (ns)
 		 * @param date A date object of when the art was produced
 		 * @param isExplicit Whehter the art is explicit
 		 */
-		constructor(title, artLink, characters, artists, date, isExplicit)
+		constructor(title, artLink, characters, artists, date, description, isExplicit)
 		{
 			this.frameId = null;
 			this.__title = title;
@@ -389,6 +391,7 @@ registerNamespace("Pages.Art", function (ns)
 			this.characters = characters;
 			this.artists = artists;
 			this.date = date;
+			this.description = description;
 			this.__isExplicit = isExplicit;
 			this.filtered = false;
 		}
@@ -471,14 +474,22 @@ registerNamespace("Pages.Art", function (ns)
 			{
 				if (event.target.tagName.toLowerCase() === 'a') { return; }
 
-				Common.DOMLib.addStyle(image, { display: "block", "margin-left": "auto", "margin-right": "auto" });
+				Common.DOMLib.addStyle(image, { display: "block", "margin-left": "auto", "margin-right": "auto", "height": "100%" });
 				Common.Controls.Popups.showModal(
 					this.__title,
-					image.outerHTML,
+					`<div style="display: flex; flex-direction: row; height: 100%">`
+					+ `<div style="flex-basis: 300px; flex-shrink: 0; flex-grow: 0"><div class="card">`
+					+ `<p>${this.description || "No description"}</p></div></div>`
+					+ `${image.outerHTML}`
+					+ `</div>`,
 					{
 						width: "95%",
 						height: "95%",
 						overflow: "auto"
+					},
+					() =>
+					{
+						Common.DOMLib.addStyle(image, { "height": null });
 					}
 				);
 			};
@@ -554,6 +565,10 @@ window.onload = () =>
 			["Freya"],
 			["ChelseaRhi"],
 			new Date(2022, 01, 24),
+			"A depiction of my Human Paladin in a Tomb of Annhilation campaign I was in for years.<br /><br />"
+			+ " Once driven from her home, Freya returned to Chult to cleanse it of the undead menace."
+			+ " Tough but fair, Freya is a kind soul who is extremely protective of children in particular.<br /><br />"
+			+ " News of her death has been greatly exaggerated.",
 			false
 		),
 		new ArtFrame(
@@ -562,6 +577,8 @@ window.onload = () =>
 			["Nocturna"],
 			["Vera"],
 			new Date(2022, 06, 18),
+			"A cross stitch project I completed as a gift for my partner.<br /><br />"
+			+ "I am never stitching a white background again - it took forever and was not worth it!",
 			false
 		),
 		new ArtFrame(
@@ -570,6 +587,12 @@ window.onload = () =>
 			["Eryn", "Serin", "Ghodukk", "Lightsong"],
 			["BastienAufrere"],
 			new Date(2019, 03, 25),
+			"The Risen D&D Party as they once appeared years ago, before some could rise no more.<br /><br />"
+			+ " Beginning at the left and moving clockwise, we have:<br /><br />"
+			+ "- Eryn, my original character, a Half-Elf ranger. He is succeeded by Vera.<br /><br />"
+			+ "- Lightsong, a Dwarven cleric who yet lives.<br /><br />"
+			+ "- Serin, a Dragonborn bard who yet lives.<br /><br />"
+			+ "- Ghodukk, a Human barbarian, who tragically fell at the culmination of his story.",
 			false
 		),
 		new ArtFrame(
@@ -578,6 +601,13 @@ window.onload = () =>
 			["Vera", "Serin", "Luric", "Lightsong", "Percy"],
 			["ShrimpLoverCat"],
 			new Date(2022, 02, 12),
+			"The Risen D&D party and their patron enjoying a light brunch between adventures.<br /><br />"
+			+ "From left to right:<br /><br />"
+			+ "- Lightsong, a Dwarven cleric<br /><br />"
+			+ "- Vera, a smug Tiefling wizard<br /><br />"
+			+ "- Serin, a Dragonborn bard<br /><br />"
+			+ "- Luric, a Half-Elf warlock<br /><br />"
+			+ "- Percy, an even more smug High-Elf wizard",
 			false
 		),
 		new ArtFrame(
@@ -586,6 +616,7 @@ window.onload = () =>
 			["Sindri"],
 			["Despey"],
 			new Date(2016, 08, 22),
+			"A piece I won from a raffle almost as soon as I adopted Sindri's design! Something has perplexed the poor lad.",
 			false
 		),
 		new ArtFrame(
@@ -594,6 +625,7 @@ window.onload = () =>
 			["Sindri"],
 			["Raiyk"],
 			new Date(2018, 03, 12),
+			"There are lots of things to love about Andromeda, not least of which is the exciting new plethora of flora to raise and study!",
 			false
 		),
 		new ArtFrame(
@@ -602,6 +634,7 @@ window.onload = () =>
 			["Sindri"],
 			["Despey"],
 			new Date(2016, 10, 19),
+			"During his time on Earth, Sindri would certainly have tried a cowboy hat or two.",
 			false
 		),
 		//new ArtFrame(
@@ -610,14 +643,17 @@ window.onload = () =>
 		//	["Sindri"],
 		//	["Palavenmoons"],
 		//	new Date(2017, 09, 25),
+		//	"",
 		//	true
 		//),
 		new ArtFrame(
-			"Tangled Up in Blue",
+			"Tangled up in Blue",
 			"../img/Sindri Profile - jesterdk & Vera - 2016-10-14.png",
 			["Sindri"],
 			["Vera", "JesterDK"],
 			new Date(2016, 09, 14),
+			"JesterDK was kind enough to post some free-to-use Turian lines I filled in!<br /><br />" +
+			"Background is from my at-the-time musical hyperfixation, Blood on the Tracks",
 			false
 		),
 		new ArtFrame(
@@ -626,6 +662,8 @@ window.onload = () =>
 			["Sindri"],
 			["Raiyk"],
 			new Date(2016, 08, 10),
+			"A reference of Sindri designed by Raiyk as an adopt!<br /><br />"
+			+ "I really love his ombre-fringe design - I've written it into his backstory as a subtle regional tattoo pattern.",
 			false
 		),
 		new ArtFrame(
@@ -634,6 +672,8 @@ window.onload = () =>
 			["Vera"],
 			["ShrimpLoverCat"],
 			new Date(2021, 11, 08),
+			"A wonderfully shaded piece from Catty!<br /><br />"
+			+ "Hmm, I wonder who she's scrying on...",
 			false
 		),
 		new ArtFrame(
@@ -642,14 +682,17 @@ window.onload = () =>
 			["Vera", "Jack"],
 			["ShrimpLoverCat"],
 			new Date(2021, 07, 22),
+			"Vera posing for a photo with her non-canonical sibling, my real-sibling's character Jack!<br /><br />"
+			+ "In canon, I bet they would (playfully!) fight as much as we do.",
 			false
 		),
 		new ArtFrame(
-			"This Better Be Good",
+			"This Better be Good",
 			"../img/Vera Coffee - Chelsea Rhi - 2022-01-23.png",
 			["Vera"],
 			["ChelseaRhi"],
 			new Date(2022, 0, 23),
+			"A wonderful icon commission I got from Chelsea-Rhi on twitter. Do not mess with Vera before she's had her coffee!",
 			false
 		),
 		//new ArtFrame(
@@ -658,6 +701,7 @@ window.onload = () =>
 		//	["Vera"],
 		//	["BonnieGuerra"],
 		//	new Date(2022, 03, 22),
+		//	"",
 		//	true
 		//),
 		new ArtFrame(
@@ -666,6 +710,9 @@ window.onload = () =>
 			["Vera"],
 			["BereniceBoggrefe"],
 			new Date(2021, 01, 11),
+			"This is the very first piece of Vera I ever commissioned!<br /><br />"
+			+ "Berenice only had some found reference images to work with and effectively created Vera's canon look from them.<br /><br />"
+			+ "I consider this to be the authoritative depiction of Vera.",
 			false
 		),
 	]);
