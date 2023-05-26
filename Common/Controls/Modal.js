347accca57d89109f6ae228b2d46540501c977ab
GroundedWren <120978593+GroundedWren@users.noreply.github.com>
2023-05-26 01:53:05 -0500

    Hot fix

---
+++ Common/Controls/Modal.js
@@ -0,0 +1,137 @@
+﻿/**
+ * Code for a singleton modal popup which lives directly in the Popups namespace. 
+ */
+registerNamespace("Common.Controls.Popups", function (ns)
+{
+	ns.SUBVEIL_ID = "subveil";
+
+	ns.isModalShowing = false;
+
+	//#region private fields
+	// The veil upon which our modal sits
+	var __veilEl = null;
+	// The top-level DOM element of the modal
+	var __modalEl = null;
+
+	// The modal's title element
+	var __modalTitle = null;
+	// The modal's content element
+	var __modalContent = null;
+
+	//Callback for when the modal is closed
+	var __onModalClosed = function () { };
+	//#endregion
+
+	/**
+	 * Shows a modal popup
+	 * @param title The modal's title
+	 * @param content The modal's HTML content
+	 * @param style Style attributes to apply to the modal
+	 * @param onClosed Callback when the modal closes
+	 */
+	function showModal(title, content, style, onClosed)
+	{
+		__createDOM();
+		Common.DOMLib.addStyle(__modalEl, style);
+
+		__modalTitle.innerHTML = title;
+		__modalContent.innerHTML = content;
+
+		__modalEl.focus();
+
+		document.body.addEventListener('keydown', __hideModalOnEsc);
+		document.body.classList.add('stop-scrolling');
+
+		get_subveil().setAttribute("inert", "true");
+
+		__onModalClosed = onClosed || function () { };
+
+		ns.isModalShowing = true;
+	};
+	ns.showModal = showModal;
+
+	function __createDOM()
+	{
+		const { el: veil } = Common.DOMLib.createElement(
+			"div",
+			window.document.body,
+			["veil"]
+		);
+		__veilEl = veil;
+
+		const { el: modalEl } = Common.DOMLib.createElement(
+			"div",
+			veil,
+			["modal", "popup"]
+		);
+		modalEl.setAttribute("aria-live", "polite");
+		__modalEl = modalEl;
+
+		const { el: modalHeader } = Common.DOMLib.createElement(
+			"div",
+			modalEl,
+			["popup-header"]
+		);
+
+		const { el: modalTitle } = Common.DOMLib.createElement(
+			"span",
+			modalHeader,
+			["popup-title"]
+		);
+		Common.DOMLib.setAttributes(modalTitle, {
+			"role": "heading",
+			"aria-level": "1"
+		});
+		__modalTitle = modalTitle;
+
+		const { el: modalClose } = Common.DOMLib.createElement(
+			"button",
+			modalHeader,
+			["popup-close"]
+		);
+		modalClose.setAttribute("aria-label", "Close the modal");
+		modalClose.onclick = () => { hideModal(); };
+		modalClose.innerHTML = "<span>&times;</span>";
+
+		const { el: modalContent } = Common.DOMLib.createElement(
+			"div",
+			modalEl,
+			["popup-content"]
+		);
+		__modalContent = modalContent;
+	}
+
+	/**
+	 * Hides the modal, completely removing it from the DOM
+	 */
+	function hideModal()
+	{
+		__veilEl.remove();
+
+		document.body.removeEventListener('keydown', __hideModalOnEsc);
+		document.body.classList.remove('stop-scrolling');
+
+		get_subveil().removeAttribute("inert");
+
+		ns.isModalShowing = false;
+		__onModalClosed();
+	};
+	ns.hideModal = hideModal;
+
+	/**
+	 * Gets the subveil element
+	 */
+	function get_subveil()
+	{
+		return document.getElementById(ns.SUBVEIL_ID);
+	}
+	ns.get_subveil = get_subveil;
+
+	function __hideModalOnEsc(keyEv)
+	{
+		if (keyEv.keyCode === Common.KeyCodes.Esc)
+		{
+			hideModal();
+		}
+	};
+});
---
+++ Common/Controls/PageControl.js
@@ -0,0 +1,207 @@
+﻿/**
+ * Namespace for a page control (tabbable pages)
+ */
+registerNamespace("Common.Controls.PageControl", function (ns)
+{
+	/**
+	 * Namespace function to build a new page control
+	 */
+	function buildPageControl(parentEl, zeroStateMessage, name)
+	{
+		const { el: pageControl } = Common.DOMLib.createElement(
+			"div",
+			parentEl,
+			["page-control"]
+		);
+
+		const { el: tabStrip } = Common.DOMLib.createElement(
+			"div",
+			pageControl,
+			["tab-strip"]
+		);
+		Common.DOMLib.createElement(
+			"div",
+			tabStrip,
+			["tab-gutter"]
+		);
+
+		const { el: pageContainer } = Common.DOMLib.createElement(
+			"div",
+			pageControl,
+			["page-container"]
+		);
+		return new PageControl(pageControl, tabStrip, pageContainer, {}, zeroStateMessage, name);
+	};
+	ns.buildPageControl = buildPageControl;
+
+	/**
+	 * Primary class to manage a page control
+	 */
+	class PageControl
+	{
+		//top-level container el
+		controlEl;
+		//container to hold the tabs
+		__tabStripEl;
+		//container to hold the pages
+		__pageContainerEl;
+		//mapping from tab element ids to Tab objects
+		__tabDict;
+		//zero state control element
+		__zeroStateControl;
+
+		//element id of the currently selected tab
+		__activeTabId;
+		//element if of the rightmost tab in the tabstrip
+		__lastTabId;
+
+		/**
+		 * Create a page control from existing DOM elements
+		 */
+		constructor(pageControl, tabStrip, pageContainer, tabPageMap, zeroStateMessage, name)
+		{
+			this.controlEl = pageControl;
+			this.__tabStripEl = tabStrip;
+			this.__pageContainerEl = pageContainer;
+
+			this.controlEl.setAttribute("role", "region");
+			this.controlEl.setAttribute("aria-label", (name || "") + " page control");
+			this.__pageContainerEl.setAttribute("aria-live", "polite");
+
+			this.__tabDict = {};
+			for (var tabId of Object.keys(tabPageMap))
+			{
+				this.registerTab(document.getElementById(tabId), tabPageMap[tabId]);
+				this.__lastTabId = tabId;
+			}
+
+			if (zeroStateMessage != undefined)
+			{
+				this.__zeroStateControl = Common.Controls.ZeroState.embedZSC(this.__pageContainerEl, zeroStateMessage);
+			}
+
+			this.__activeTabId = null;
+
+			this.controlEl.classList.remove("loading");
+		}
+
+		/**
+		 * Creates a new tab and associates it with the page
+		 */
+		addNewTab(tabText, page, onActivate)
+		{
+			const { el: newTabEl, id: newTabId } = Common.DOMLib.createElement(
+				"div",
+				undefined,
+				["tab-strip-tab", "button-like"]
+			);
+			newTabEl.innerHTML = `<span>${tabText}</span>`;
+
+			if (this.__lastTabId != null)
+			{
+				this.__tabDict[this.__lastTabId].tabEl.after(newTabEl);
+				this.__lastTabId = newTabId;
+			}
+			else
+			{
+				this.__tabStripEl.prepend(newTabEl)
+				this.__lastTabId = newTabId;
+			}
+
+			this.__pageContainerEl.appendChild(page);
+			page.classList.add("page-control-page")
+			this.registerTab(newTabEl, page, onActivate);
+
+			return newTabId;
+		};
+
+		/**
+		 * Adds an onActivate handler to a tab
+		 */
+		addOnActivate(tabId, onActivate)
+		{
+			if (this.__tabDict[tabId] == null) { return; }
+
+			this.__tabDict[tabId].onActivate = onActivate;
+		}
+
+		/**
+		 * Registers a tab so that clicking it works
+		 */
+		registerTab(tab, page, onActivate)
+		{
+			this.__tabDict[tab.id] = new Tab(tab, page, onActivate);
+			
+			Common.DOMLib.setAsButton(tab, Common.fcd(this, this.setActiveTab, [tab.id]));
+		}
+
+		/**
+		 * Tab pages
+		 * @param tabId id of the tab to activate
+		 */
+		setActiveTab(tabId, event)
+		{
+			if (this.__tabDict[tabId] == null) { return; }
+
+			if (this.__activeTabId !== null)
+			{
+				this.__tabDict[this.__activeTabId].deactivate();
+			}
+
+			this.__exitZeroState();
+
+			this.__tabDict[tabId].activate();
+			this.__activeTabId = tabId;
+
+			if (event) { event.preventDefault(); }
+		};
+
+		__exitZeroState()
+		{
+			if (this.__zeroStateControl)
+			{
+				Common.DOMLib.addStyle(this.__zeroStateControl, { "display": "none" });
+			}
+		}
+	};
+	ns.PageControl = PageControl;
+
+	/**
+	 * Convenience class to represent one tab
+	 */
+	class Tab
+	{
+		tabEl;
+		__pageEl;
+		onActivate;
+		constructor(tabEl, pageEl, onActivate)
+		{
+			this.tabEl = tabEl;
+			this.__pageEl = pageEl;
+			this.onActivate = onActivate;
+
+			tabEl.setAttribute("aria-pressed", "false");
+		}
+
+		/**
+		 * Make the tab's page active
+		 */
+		activate()
+		{
+			if (this.onActivate) { this.onActivate(); }
+			this.tabEl.classList.add("selected");
+			this.__pageEl.classList.add("selected");
+			this.tabEl.setAttribute("aria-pressed", "true");
+		};
+
+		/**
+		 * Make the tab's page inactive
+		 */
+		deactivate()
+		{
+			this.tabEl.classList.remove("selected");
+			this.__pageEl.classList.remove("selected");
+			this.tabEl.setAttribute("aria-pressed", "true");
+		};
+	};
+});
